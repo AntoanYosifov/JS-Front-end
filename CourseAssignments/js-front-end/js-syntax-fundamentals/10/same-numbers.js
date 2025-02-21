cf7927b6db8e3fa26a391c8131b9e7f2269b193b
AntoanYosifov <tapaktapxaomi@gmail.com>
2025-02-21 09:07:16 +0100

    road-radar

---
+++ CourseAssignments/js-front-end/js-syntax-fundamentals/10/same-numbers.js
@@ -0,0 +1,23 @@
+function solve(number) {
+
+  let stringNumber = number.toString();
+
+  let same = true;
+  let sum = 0;
+
+  for (let i = 0; i < stringNumber.length; i++) {
+    sum += Number(stringNumber[i]);
+
+    if (i > 0) {
+      if (stringNumber[i] !== stringNumber[i - 1]) {
+        same = false;
+      }
+    }
+  }
+
+  console.log(same);
+  console.log(sum);
+
+}
+
+solve(1234);
