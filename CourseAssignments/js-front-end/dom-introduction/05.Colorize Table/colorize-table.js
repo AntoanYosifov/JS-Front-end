a573ab5a54c5724da42ce6e1f79d739188ef1b46
AntoanYosifov <tapaktapxaomi@gmail.com>
2025-03-10 13:22:01 +0100

    colorize-table.js

--- CourseAssignments/js-front-end/dom-introduction/05.Colorize Table/colorize-table.js
+++ CourseAssignments/js-front-end/dom-introduction/05.Colorize Table/colorize-table.js
@@ -1,3 +1,4 @@
 function colorize() {
-    //TODO
+    let tableRowElements = document.querySelectorAll('table tbody tr:nth-child(even)');
+    tableRowElements.forEach(el => el.style.backgroundColor = 'teal');
 }
