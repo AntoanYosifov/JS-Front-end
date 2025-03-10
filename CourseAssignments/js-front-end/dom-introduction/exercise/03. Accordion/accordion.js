9b59d27647a9ee70576af51f70b9d1f9afaa191b
AntoanYosifov <tapaktapxaomi@gmail.com>
2025-03-10 15:08:46 +0100

    accordion.js

--- CourseAssignments/js-front-end/dom-introduction/exercise/03. Accordion/accordion.js
+++ CourseAssignments/js-front-end/dom-introduction/exercise/03. Accordion/accordion.js
@@ -1,3 +1,14 @@
 function toggle() {
-    //TODO
+    let buttonElement = document.querySelector('.head span.button');
+    let buttonText = buttonElement.textContent;
+    let extraDivElement = document.getElementById('extra');
+
+    if(buttonText === 'More') {
+        buttonElement.textContent = 'Less';
+        extraDivElement.style.display = 'block';
+    } else {
+        buttonElement.textContent = 'More';
+        extraDivElement.style.display = 'none';
+    }
+
 }
