9e843cbf252d82bab278552ca6eafa52f337aae0
AntoanYosifov <tapaktapxaomi@gmail.com>
2025-03-10 13:35:55 +0100

    sum-table.js

--- CourseAssignments/js-front-end/dom-introduction/06.Sum Table/sum-table.js
+++ CourseAssignments/js-front-end/dom-introduction/06.Sum Table/sum-table.js
@@ -1,3 +1,8 @@
 function sumTable() {
-    // TODO
+     // let sum = Array.from(document.querySelectorAll('table tbody tr td:last-of-type:not(#sum)'))
+     //    .reduce((sum, priceElement) => sum + Number(priceElement.textContent) , 0);
+
+    document.getElementById('sum').textContent = Array.from(document.querySelectorAll('table tbody tr td:last-of-type:not(#sum)'))
+        .reduce((sum, priceElement) => sum + Number(priceElement.textContent) , 0);
+
 }
