d5c8a571e17f72f5f3a52eb59e78131783168e49
AntoanYosifov <tapaktapxaomi@gmail.com>
2025-03-10 14:58:04 +0100

    pascal-or-camel-case.js

--- CourseAssignments/js-front-end/dom-introduction/exercise/02. Pascal or Camel Case/pascal-or-camel-case.js
+++ CourseAssignments/js-front-end/dom-introduction/exercise/02. Pascal or Camel Case/pascal-or-camel-case.js
@@ -1,3 +1,27 @@
 function solve() {
-  //TODO
+    let inputText = document.getElementById('text').value.toLowerCase();
+    let currentCase = document.getElementById('naming-convention').value;
+
+    let clearCommand = currentCase.replace(/\s+/g, "").toLowerCase();
+
+    let words = inputText.split(' ');
+    let transformed = '';
+    switch (clearCommand) {
+        case 'camelcase':
+            for (let i = 1; i < words.length; i++) {
+                words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+            }
+            transformed = words.join('');
+            break
+        case 'pascalcase':
+            for (let i = 0; i < words.length; i++) {
+                words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+            }
+            transformed = words.join('');
+            break
+        default:
+            transformed = 'Error!'
+    }
+
+    document.getElementById('result').textContent = transformed;
 }
