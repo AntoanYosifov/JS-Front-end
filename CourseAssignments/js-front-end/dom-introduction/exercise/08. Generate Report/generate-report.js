8bbd4a07784c90e7031e4991e1fa7e4e19f2bafe
AntoanYosifov <tapaktapxaomi@gmail.com>
2025-03-13 14:43:59 +0100

    generate-report & number-convertor

--- CourseAssignments/js-front-end/dom-introduction/exercise/08. Generate Report/generate-report.js
+++ CourseAssignments/js-front-end/dom-introduction/exercise/08. Generate Report/generate-report.js
@@ -1,3 +1,36 @@
 function solve() {
-    //TODO
+    let checkboxes = document.querySelectorAll('thead input[type=checkbox]');
+    let tableRows = document.querySelectorAll('tbody tr');
+
+    let checkedBoxesNames = []
+    let checkedBoxesIndices = []
+    checkboxes.forEach((box, index) => {
+        if(box.checked) {
+            checkedBoxesNames.push(box.name);
+            checkedBoxesIndices.push(index)
+        }
+    });
+
+    let allRowData = [];
+    tableRows.forEach(row => {
+        let rowData = [];
+        checkedBoxesIndices.forEach(index => {
+            rowData.push(row.children[index].textContent.trim())
+        });
+        allRowData.push(rowData);
+    });
+
+    let reportArr = [];
+    for (let i = 0; i < allRowData.length; i++) {
+        let currentInfo = allRowData[i];
+
+        let currentInfoObj = {};
+        for (let j = 0; j < currentInfo.length; j++) {
+            currentInfoObj[checkedBoxesNames[j]] = currentInfo[j];
+        }
+
+        reportArr.push(currentInfoObj)
+    }
+
+    document.querySelector('textarea#output').value = JSON.stringify(reportArr);
 }
--- CourseAssignments/js-front-end/dom-introduction/exercise/09. Number Convertor/number-convertor.js
+++ CourseAssignments/js-front-end/dom-introduction/exercise/09. Number Convertor/number-convertor.js
@@ -1,3 +1,18 @@
 function solve() {
-    // TODO
+    const selectElement = document.getElementById('selectMenuTo');
+    const numberToConvert = Number(document.querySelector('input[type=number]#input').value);
+
+    const convertTo = selectElement.value;
+
+    function convertNumber(number, command) {
+        switch (command) {
+            case 'binary':
+                return number.toString(2);
+
+            case 'hexadecimal':
+                return number.toString(16).toUpperCase();
+        }
+    }
+
+    document.getElementById('result').value = convertNumber(numberToConvert, convertTo);
 }
