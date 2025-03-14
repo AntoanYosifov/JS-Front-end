db3dbed78edd6e35122842facdf08459233ddc6e
AntoanYosifov <tapaktapxaomi@gmail.com>
2025-03-14 16:37:10 +0100

    event handling demos

--- CourseAssignments/js-front-end/dom-manipulation/lab/02. Delete-From-Table/app.js
+++ CourseAssignments/js-front-end/dom-manipulation/lab/02. Delete-From-Table/app.js
@@ -1,3 +1,21 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const emailElements = document.querySelectorAll('table tbody tr td:last-child');
+    const emailToDelete = document.querySelector('input[name=email]').value;
+    const resultElement = document.getElementById('result');
+
+    let isDeleted = false;
+
+    emailElements.forEach(el => {
+        if (el.textContent === emailToDelete.trim()) {
+            el.parentElement.remove()
+            isDeleted = true;
+        }
+    });
+
+    if (isDeleted) {
+        resultElement.textContent = 'Deleted.';
+    } else {
+        resultElement.textContent = 'Not found.'
+    }
 }
+
