90e168d44b5bc0a8dc5db05cab4b7c6f42e0db77
AntoanYosifov <tapaktapxaomi@gmail.com>
2025-03-15 10:11:45 +0100

    Add / Delete

--- CourseAssignments/js-front-end/dom-manipulation/lab/03. Add-Delete/app.js
+++ CourseAssignments/js-front-end/dom-manipulation/lab/03. Add-Delete/app.js
@@ -1,3 +1,21 @@
 function addItem() {
-    //TODO...
+    const itemsElement = document.getElementById('items');
+    const newItemText = document.querySelector('#newItemText');
+
+    console.log(newItemText.value);
+    const newLiElement = document.createElement('li');
+    const newAnchorElement = document.createElement('a');
+    newAnchorElement.href = '#';
+    newAnchorElement.textContent = '[Delete]'
+
+    newAnchorElement.addEventListener('click', (e) => {
+        e.currentTarget.parentElement.remove();
+    })
+
+    newLiElement.textContent = newItemText.value;
+    newLiElement.appendChild(newAnchorElement);
+    itemsElement.appendChild(newLiElement);
+
+
+    newItemText.value = '';
 }
