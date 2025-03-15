0a7847b63aa0c35687962bae2563da443f496df4
AntoanYosifov <tapaktapxaomi@gmail.com>
2025-03-15 10:33:19 +0100

    mouse gradient

--- CourseAssignments/js-front-end/dom-manipulation/lab/04. Mouse-Gradient/app.js
+++ CourseAssignments/js-front-end/dom-manipulation/lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,15 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const resultElement = document.getElementById('result');
+    const gradientElement = document.getElementById('gradient');
+
+    gradientElement.addEventListener('mousemove', (e) => {
+
+        const currentPosition = e.offsetX;
+        const elementWidth = e.currentTarget.clientWidth;
+        console.log(e.currentTarget.clientWidth);
+
+        const percent = Math.floor((currentPosition / elementWidth) * 100);
+
+        resultElement.textContent = `${percent}%`;
+    })
 }
