df277c52c0a80b8f6fffa0075c111761442f0196
AntoanYosifov <tapaktapxaomi@gmail.com>
2025-03-17 08:40:15 +0100

    dynamic validation

--- CourseAssignments/js-front-end/dom-manipulation/lab/06. Dynamic-Validation/app.js
+++ CourseAssignments/js-front-end/dom-manipulation/lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,10 @@
 function validate() {
-    console.log('TODO:...');
+    const emailInputElement = document.getElementById('email');
+    const pattern = /[a-z]+@[a-z]+\.[a-z]+/
+    emailInputElement.addEventListener('change', (e) => {
+        if (!pattern.test(e.currentTarget.value)) {
+            return e.currentTarget.classList.add('error');
+        }
+        e.currentTarget.classList.remove('error');
+    });
 }
