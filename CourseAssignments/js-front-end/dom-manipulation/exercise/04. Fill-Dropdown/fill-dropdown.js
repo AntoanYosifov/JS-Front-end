61a36a82258f7cb01bc737867934cae1703d0ea0
AntoanYosifov <tapaktapxaomi@gmail.com>
2025-03-17 18:18:35 +0100

    fill-dropdown

--- CourseAssignments/js-front-end/dom-manipulation/exercise/04. Fill-Dropdown/fill-dropdown.js
+++ CourseAssignments/js-front-end/dom-manipulation/exercise/04. Fill-Dropdown/fill-dropdown.js
@@ -1,5 +1,24 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    const selectElement = document.getElementById('menu');
+    const formElement = document.querySelector('form');
+
+    formElement.addEventListener('submit', handleEvent);
+
+    function handleEvent(e) {
+        e.preventDefault();
+
+        const newItemTextEl = e.currentTarget.querySelector('input#newItemText');
+        const newItemValueEl = e.currentTarget.querySelector('input#newItemValue');
+
+        const optionToADd = document.createElement('option');
+        optionToADd.textContent = newItemTextEl.value;
+        optionToADd.value = newItemValueEl.value;
+
+        selectElement.appendChild(optionToADd);
+
+        newItemTextEl.value = '';
+        newItemValueEl.value = '';
+    }
 }
