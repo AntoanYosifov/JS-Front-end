7669a840c56ba15bba252d075a88b1b0d02a2fb7
AntoanYosifov <tapaktapxaomi@gmail.com>
2025-03-20 11:27:30 +0100

    encode-and-decode-messages

--- CourseAssignments/js-front-end/dom-manipulation/exercise/05. Encode-and-Decode-Messages/encode-and-decode-messages.js
+++ CourseAssignments/js-front-end/dom-manipulation/exercise/05. Encode-and-Decode-Messages/encode-and-decode-messages.js
@@ -1,5 +1,31 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    const encodeFormEl = document.getElementById('encode');
+    const decodeFormEl = document.getElementById('decode');
+
+    const encodeTextAreaEl = encodeFormEl.querySelector('textarea');
+    const decodeTextAreaEl = decodeFormEl.querySelector('textarea');
+
+    encodeFormEl.addEventListener('submit', (e) => {
+        e.preventDefault();
+        let messageToEncode = encodeTextAreaEl.value;
+
+        let encodedMessage = messageToEncode
+            .split('')
+            .map(char => String.fromCharCode(char.charCodeAt(0) + 1))
+            .join('');
+
+        encodeTextAreaEl.value = '';
+        decodeTextAreaEl.value = encodedMessage;
+    });
+
+    decodeFormEl.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        decodeTextAreaEl.value = decodeTextAreaEl.value
+            .split('')
+            .map(char => String.fromCharCode(char.charCodeAt(0) - 1))
+            .join('');
+    });
 }
