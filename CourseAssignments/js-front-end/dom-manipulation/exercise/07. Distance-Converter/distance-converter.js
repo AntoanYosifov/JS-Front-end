a62cca857f622f035225ca137041271578fe25f4
AntoanYosifov <tapaktapxaomi@gmail.com>
2025-03-21 10:24:31 +0100

    distance-converter.js

--- CourseAssignments/js-front-end/dom-manipulation/exercise/07. Distance-Converter/distance-converter.js
+++ CourseAssignments/js-front-end/dom-manipulation/exercise/07. Distance-Converter/distance-converter.js
@@ -1,5 +1,39 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+
+    const values = {
+        km: 1000,
+        m: 1,
+        cm: 0.01,
+        mm: 0.001,
+        mi: 1609.34,
+        yrd: 0.9144,
+        ft: 0.3048,
+        in: 0.0254,
+    }
+
+    const convertButton = document.getElementById('convert');
+
+    convertButton.addEventListener('click', handleConvert);
+
+    function handleConvert() {
+        const inputDistance = Number(document.getElementById('inputDistance').value);
+
+        const inputUnitsSelectEl = document.getElementById('inputUnits');
+        const selectedUnitFrom = inputUnitsSelectEl.children[inputUnitsSelectEl.selectedIndex].value;
+        const multiplier = values[selectedUnitFrom];
+
+        const distanceInMeters = inputDistance * multiplier;
+
+        const outputUnitsSelectEl = document.getElementById('outputUnits');
+        const outputUnitTo = outputUnitsSelectEl.children[outputUnitsSelectEl.selectedIndex].value;
+
+        document.getElementById('outputDistance').value = convert(distanceInMeters, outputUnitTo);
+
+    }
+
+    function convert(distanceInMeters , convertToUnits) {
+        return distanceInMeters / values[convertToUnits];
+    }
 }
